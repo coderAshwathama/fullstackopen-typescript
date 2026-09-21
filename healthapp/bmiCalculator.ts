f5bc66c23e93af272d372bac2a57961427bdbb16
Rijan Tiwari <reezantiwaree@gmail.com>
2026-09-21 06:54:29 +0545

    changed the Bmi calculator

--- healthapp/bmiCalculator.ts
+++ healthapp/bmiCalculator.ts
@@ -1,15 +1,15 @@
-const calcualteBmi = (height: number, weight: number) => {
+const calcualteBmi = (height: number, weight: number): string => {
   const heightInMeters = height / 100;
   const bmi = weight / heightInMeters ** 2;
   if (bmi < 16.0) return "Underweight (Severe thinness)";
-  if (bmi >= 16.0 && bmi <= 17.0) return "Underweight (Moderate thinness)";
+  if (bmi < 17.0) return "Underweight (Moderate thinness)";
 
-  if (bmi >= 17.0 && bmi <= 18.5) return "Underweight (Mild thinness)";
-  if (bmi >= 18.5 && bmi <= 25.0) return "Normal range";
-  if (bmi >= 25.0 && bmi <= 30.0) return "Overweight (Pre-obese)";
-  if (bmi >= 30.0 && bmi <= 35.0) return "Obese (Class I)";
-  if (bmi >= 35.0 && bmi <= 40.0) return "Obese (Class II)";
-  if (bmi >= 40.0) return "Obese (Class III)";
+  if (bmi < 18.5) return "Underweight (Mild thinness)";
+  if (bmi < 25.0) return "Normal range";
+  if (bmi < 30.0) return "Overweight (Pre-obese)";
+  if (bmi < 35.0) return "Obese (Class I)";
+  if (bmi < 40.0) return "Obese (Class II)";
+  return "Obese (Class III)";
 };
 
-console.log(calcualteBmi(180, 74));
+console.log(calcualteBmi(182, 45));
